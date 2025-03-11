9b95d062b6991aac40cae06f8cab29bb7051ccd1
Atharva Bhalilkar <atharvabhalilkar@gmail.com>
2025-03-11 23:39:59 +0000

    Fixed dataset download script by updating Google Drive link
    Since the dataset is too large for GitHub, i've store it on Google Drive.
    
    To download it, follow these steps:
    
    1. Navigate to the main repository folder (if not already there):
    cd /path/to/chord-gen-ai
    
    2.Run the dataset download script:
    python utils/download_data.py
    
    This will fetch the data from google drive and store it in data/raw/chordonomicon.csv

--- utils/download_data.py
+++ utils/download_data.py
@@ -1,30 +1,27 @@
-from datasets import load_dataset # type: ignore 
-import pandas as pd # type: ignore
+import gdown # type: ignore
 import os
 
-raw_data_path = "data/raw/"
-processed_data_path = "data/processed/"
-dataset_name = "ailsntua/Chordonomicon"
+file_id = "1QpwFwGbm8xAdTpMfxnBEzFNnoFougW0g"
+raw_data_path= "data/raw"
+processed_data_path = "data/processed"
+output_file = os.path.join(raw_data_path, "chordonomicon.csv")
 
 os.makedirs(raw_data_path, exist_ok=True)
 os.makedirs(processed_data_path, exist_ok=True)
 
 def fetch_and_save():
-    # fetches the dataset and saves it as a csv file 
+
+    # download dataset from Google Drive using gdown.
 
     print('Fetching dataset...')
 
-    # load dataset from Hugging Face
-    dataset = load_dataset(dataset_name)
+    # google drive url that contains the dataset
+    url = 'https://drive.google.com/file/d/1QpwFwGbm8xAdTpMfxnBEzFNnoFougW0g/view?usp=drive_link'
 
-     # convert to Pandas DataFrame
-    df = pd.DataFrame(dataset["train"])
+    # download using gdown with fuzzy option
+    gdown.download(url, output_file, quiet=False, fuzzy=True)
 
-    # store the data as a csv file
-    raw_file_path = os.path.join(raw_data_path, "chordonomicon.csv")
-    df.to_csv(raw_file_path, index=False) 
-
-    print(f'Dataset fetched and stored at {raw_data_path}')
+    print(f'Dataset fetched and stored at {output_file}')
 
 if __name__ == "__main__":
     fetch_and_save()
